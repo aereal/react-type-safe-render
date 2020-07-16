512c005e2fd11f52db2de3e6e59568e44ef30216
aereal <aereal@aereal.org>
2020-07-16 18:20:31 +0900

    build

---
+++ packages/@aereal/react-type-safe-render/src/index.ts
@@ -0,0 +1,21 @@
+import { render } from 'react-dom';
+import type { DOMElement, DOMAttributes, SFCElement, Component, ComponentState, CElement, ReactElement } from 'react';
+
+type SafeContainer = Element | DocumentFragment;
+type Callback = () => void;
+
+interface SafeRenderer {
+  <T extends Element>(element: DOMElement<DOMAttributes<T>, T>, container: SafeContainer, callback?: Callback): void;
+  (element: Array<DOMElement<DOMAttributes<any>, any>>, container: SafeContainer, callback?: Callback): void;
+  (element: SFCElement<any> | Array<SFCElement<any>>, container: SafeContainer, callback?: Callback): void;
+  <P, T extends Component<P, ComponentState>>(
+    element: CElement<P, T>,
+    container: SafeContainer,
+    callback?: Callback
+  ): void;
+  (element: Array<CElement<any, Component<any, ComponentState>>>, container: SafeContainer, callback?: Callback): void;
+  <P>(element: ReactElement<P>, container: SafeContainer, callback?: Callback): void;
+  (element: ReactElement[], container: SafeContainer, callback?: Callback): void;
+}
+
+export const safeRender: SafeRenderer = render;
